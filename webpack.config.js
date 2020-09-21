e9353c9b82e3ef276dffec6cfd9b105099e4cf1e
Andrew Warr <andywarr@macbook-pro.lan>
2020-09-21 11:52:17 -0700

    Updated path

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'client/dist/js'),
     filename: "[name].js",
-    publicPath: '/js'
+    publicPath: '/static/js'
   },
   plugins: [
     new HtmlWebPackPlugin({
